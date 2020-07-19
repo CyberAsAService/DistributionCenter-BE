802b70a6387a2a593c62bf7ce880f1f8871ea988
Tal Shafir <tal.4000@gmail.com>
2020-07-19 14:52:19 +0300

    added trying to use pypsexec and added requirements file

--- test.py
+++ test.py
@@ -3,6 +3,8 @@ from typing import Union, List, Tuple, Any
 
 from endpoint import Endpoint
 
+from pypsexec.client import Client
+
 WMI_FORMAT = 'wmic /user:{user} /password:{password} /node:{remote_host} process call create '
 PSEXEC_FORMAT = 'PsExec.exe \\\\{remote_host} -u {remote_host}\\{user} -p {password} -accepteula -h {process} \"{command}\" '
 
@@ -39,3 +41,22 @@ def run_command(remote_host: Union[Endpoint, List[Endpoint]], process: str, comm
 
 if __name__ == '_main_':
     print(run_command(Endpoint('192.168.182.129', 'Tal', 'babygirl'), 'powershell.exe', 'echo kaki > C:\\moshe.txt'))
+
+    c = Client("hostname", username="username", password="password")
+    c.connect()
+    try:
+        c.create_service()
+
+        # After creating the service, you can run multiple exe's without
+        # reconnecting
+
+        # run a simple cmd.exe program with arguments
+        print(c.run_executable("cmd.exe",
+                               arguments="/c echo Hello World"))
+
+        # run whoami.exe as the SYSTEM account
+        print(c.run_executable("whoami.exe", run_elevated=True))
+
+    finally:
+        c.remove_service()
+        c.disconnect()
