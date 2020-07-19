71a8393a7f09bbcf0696a5417c1c8a546d0cf014
Tal Shafir <tal.4000@gmail.com>
2020-07-19 15:30:41 +0300

    changed run_command to use pypsexec

--- deploy.py
+++ deploy.py
@@ -5,36 +5,27 @@ from endpoint import Endpoint
 
 from pypsexec.client import Client
 
-def run_command(remote_host: Union[Endpoint, List[Endpoint]], process: str, command: str) -> List[Tuple[int, Any]]:
-    """
-    :param remote_host:
-    :param process:
-    :param command:
-    :return:
-    """
-    if type(remote_host) == Endpoint:
-        remote_host = [remote_host]
 
-    processes = [_run_subprocess(host, process, command) for host in remote_host]
-    return [(process.wait(), (*process.communicate())) for process in processes]
-
-if __name__ == '__main__':
-
-    c = Client("192.168.182.129", username="Tal", password="babygirl", encrypt=False)
-    c.connect()
+def run_command(remote_host: Union[Endpoint, List[Endpoint]], encrypt: bool, process: str, command: str):
+    connection = Client(remote_host.ip_address, username=remote_host.username, password=remote_host.password,
+                        encrypt=encrypt)
+    connection.connect()
     try:
-        c.create_service()
-
+        connection.create_service()
         # After creating the service, you can run multiple exe's without
         # reconnecting
 
         # run a simple cmd.exe program with arguments
-        print(c.run_executable("cmd.exe",
-                               arguments="/c echo Hello World"))
+        return (connection.run_executable(process, run_elevated=True,
+                                          arguments=command))
 
-        # run whoami.exe as the SYSTEM account
-        print(c.run_executable("whoami.exe", run_elevated=True))
-
+    except Exception as err:
+        print(err)
     finally:
-        c.remove_service()
-        c.disconnect()
+        connection.remove_service()
+        connection.disconnect()
+
+
+if __name__ == '__main__':
+    print(run_command(Endpoint("192.168.182.129", username="Tal", password="babygirl"), encrypt=False,
+                process='powershell.exe', command=""""""))
