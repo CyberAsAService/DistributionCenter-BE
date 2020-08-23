360b4ff81c88bc688395d11d7a0e4edca0e513e4
Orfrid <orfridman10@gmail.com>
2020-08-23 14:12:29 +0300

    added template to Executor service

---
+++ src/services/executer/executer-controller.ts
@@ -0,0 +1,5 @@
+import * as provider from './executer-provider';
+
+export const Executer = async (params: any) => {
+    return await provider.Executer(params);
+}
--- src/services/executer/routes.ts
+++ src/services/executer/routes.ts
@@ -1,44 +1,41 @@
 import { Request, Response } from "express";
+import * as controller from "./executer-controller";
 const axios = require('axios')
-import db from "../../config/db";
 
 export default [
     {
         path: "/Executer/Status",
         method: "post",
-        handler: async (req: Request, res: Response) => {
-        let statusExecuter = (await axios.get(`http://192.168.40.130:5000/status/${req.body.task_id}`)).data;
-        if(statusExecuter["status"] == "SUCCESS")
-        {
-          //TODO -> return executer task_id from db
-          return "shit";
-        }
-        else
-        {
-          return statusExecuter;
-        }
-        }},
-
+        handler: [
+          async (req: Request, res: Response) => {
+          let statusExecuter = (await axios.get(`http://192.168.40.130:5000/status/${req.body.task_id}`)).data;
+          if(statusExecuter["status"] == "SUCCESS")
+          {
+            //TODO -> return executer task_id from db
+            return "shit";
+          }
+          else
+          {
+            return statusExecuter;
+          }
+          }]
+        },
         {
           path: "/Executer",
           method: "patch",
-          handler: async (req: Request, res: Response) => {
-            if(req.body.executer)
-            {
-              await db.none('UPDATE public."Steps" SET endtime=CURRENT_TIMESTAMP, status=${status} WHERE microtask_id = ${step_id}',
-              {
-                status: 'RUNNING',
-                step_id: req.body.task_id,
-              });
+          handler: [async (req: Request, res: Response) => {
+            let params;
+            if(req.body.executer) {
+              params = { status: 'RUNNING',
+                         step_id: req.body.task_id
+                       };
+            } else {
+              params = { status: req.body.status,
+                         step_id: req.body.task_id
+                       };
             }
-            else{
-              await db.none('UPDATE public."Steps" SET endtime=CURRENT_TIMESTAMP, status=${status} WHERE microtask_id = ${step_id}',
-              {
-                status: req.body.status,
-                step_id: req.body.task_id,
-              });
+              const data = await controller.Executer(params);
+              res.status(200).json(true);
             }
-            res.json(true);
-            }
-          },
+          ]}
 ];
