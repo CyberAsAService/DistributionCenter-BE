d3c171f0a92288c037149932fcfee285bbc4f3e0
Tal Shafir <tal.4000@gmail.com>
2020-08-24 11:44:39 +0300

    fixed return value for multiple endpoints task

--- src/services/task/routes.ts
+++ src/services/task/routes.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 const axios = require('axios')
+import { validateAddress } from "../../utils/index"
 
 export default [
   {
@@ -28,28 +29,45 @@ export default [
       // req.body.payload
       // req.body.targets
       // req.body.target_regex
-      req.body.addresses.forEach(async (address: string) => 
-      {
-        try{
-      const responsePaaS = (await axios.post('http://192.168.36.128:5000/PaaS', {
-        address: address,
-        username: req.body.username ? req.body.username : "Administrator",
-        steps: req.body.steps
-      })).data;
-      const responseExecute = (await axios.post('http://localhost:5001/execute', {
-        address: address,
-        username: 'Witcher',
-        password: 'Switcher',
-        process: 'powershell.exe',
-        command: req.body.payload,
-      })).data;
-      res.json(responseExecute);
-    } catch (error) {
-      // Passes errors into the error handler
-      console.log(error);
-    }
-    
-    });
+
+      //final response mapping {name:response}
+      let response: { [key: string]: any } = {};
+      let status = 200; // OK as default
+      req.body.addresses.forEach(async (address: string) => {
+        response[address] = { paasResponse: null, executeResponse: null };
+        if (!validateAddress(address)) {
+          // TODO determine the proper way to handle error
+          status = 422; // Unprocessable Entity 
+        } else {
+          try {
+            const responsePaaS = (await axios.post('http://192.168.36.128:5000/PaaS', {
+              address: address,
+              username: req.body.username ? req.body.username : "Administrator",
+              steps: req.body.steps
+            })).data;
+            response[address]['paasResponse'] = responsePaaS;
+
+            const responseExecute = (await axios.post('http://localhost:5001/execute', {
+              address: address,
+              username: 'Witcher',
+              password: 'Switcher',
+              process: 'powershell.exe',
+              command: req.body.payload,
+            })).data;
+            response[address]['executeResponse'] = responseExecute;
+          } catch (error) {
+            // Passes errors into the error handler
+            // TODO determine if to show error code for one failing endpoint or not
+            status = 422; // Unprocessable Entity 
+            console.log(error);
+          }
+          finally {
+            res.status(status).json(response);
+          }
+
+        }
+
+      });
     }
   },
   {
